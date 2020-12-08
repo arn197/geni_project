1d4e9dfd0e7c8fda82aa0b21c4a350a9f506e0c0
Shivam Satwah <shivamsatwah@gmail.com>
2020-12-08 15:18:21 -0500

    Thread implementation and Table for multiple requests.

--- webapp.py
+++ webapp.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from threading import Thread
 
 app = Flask(__name__)
-
+hashList = []
+passwordList = {}
+activeThreads = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
 @app.route('/crack', methods=('GET', 'POST'))
-def create():
+def crack():
     if request.method == 'POST':
-        hash = request.form['hash']
+        md5 = request.form['hash']
+        hashList.append(md5)
+        workerThread = Thread(target=new_req, args=[md5])
+        workerThread.start()
+        activeThreads[md5] = workerThread
+    return render_template('crack.html',hlength = len(hashList), hashList = hashList, passwordList = passwordList)
 
-        if not hash:
-            flash('Hash is required')
+# Server can call this to return new found password
+def receive_password(md5,password):
+    passwordList[md5] = password
 
-    return render_template('crack.html')
+
+# Mock function for new requests
+def new_req(md5):
+    a = 0
+    while a < 1000:
+        a = a + 1
+    receive_password(hashList[-1],"Password")
+    return
+
+
+
